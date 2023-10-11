6df34c1fa128cc0e37a51012d76fc1a021085b8d
Subham Ghosh <subham246@outlook.com>
2023-10-11 07:08:34 +0000

    Updated HomeController.vb

--- MVC5Application/Controllers/HomeController.vb
+++ MVC5Application/Controllers/HomeController.vb
@@ -6,7 +6,7 @@
 	End Function
 
 	Function About() As ActionResult
-		ViewData("Message") = "Your application description page."
+		ViewData("Message") = "Your application description page(updated)."
 //Updating the file
 		Return View()
 		//adding comment
